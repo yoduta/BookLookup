55d841695b6bc029010d54b6bc4c7173827d34c6
yoduta <knotinw2540@gmail.com>
2017-10-24 23:42:33 +0700

    create function

--- Booklookup.spec.js
+++ Booklookup.spec.js
@@ -1,10 +1,22 @@
+function Booklookup (AmazonServices){
+    this.AmazonServices = AmazonServices
+
+    this.search = (ispn) => {
+        let Result = this.AmazonServices
+        return Result
+    }
+}
+
+
+
+
 test('Booklookup',()=>{
     const AmazonServices = jest.fn('abcd1234')
     .mockReturnValue ({name:'Photoshop',cover:'photoshop.com',ispn:'abcd1234'})
     let app = new Booklookup (AmazonServices)
     let ispn = 'abcd1234'
     let value = app.result(ispn)
-    
+
 
 
 
