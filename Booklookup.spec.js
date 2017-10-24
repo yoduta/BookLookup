a41ff8d0c550c500ead337217ac41a5466b265ca
yoduta <knotinw2540@gmail.com>
2017-10-25 00:01:34 +0700

    create expect

--- Booklookup.spec.js
+++ Booklookup.spec.js
@@ -1,8 +1,8 @@
 function Booklookup (AmazonServices){
     this.AmazonServices = AmazonServices
 
-    this.search = (ispn) => {
-        let Result = this.AmazonServices
+    this.result = (ispn) => {
+        let Result = this.AmazonServices(ispn)
         return Result
     }
 }
@@ -12,12 +12,19 @@ function Booklookup (AmazonServices){
 
 test('Booklookup',()=>{
     const AmazonServices = jest.fn('abcd1234')
-    .mockReturnValue ({name:'Photoshop',cover:'photoshop.com',ispn:'abcd1234'})
+    .mockReturnValue ({name:'Photoshop CS7',cover:'photoshop.com',ispn:'abcd1234'})
     let app = new Booklookup (AmazonServices)
     let ispn = 'abcd1234'
     let value = app.result(ispn)
 
-
+expect(value).toEqual({name:'Photoshop CS7',cover:'photoshop.com',ispn:'abcd1234'})
+expect(AmazonServices).toHaveBeenCalled()
+expect(AmazonServices).toHaveBeenCalledWith(ispn)
+expect(value.name).toBe("Photoshop CS7")
+expect(value).toHaveProperty("cover")
+expect(value.cover).toBe("photoshop.com")
+expect(value.ispn).toBe('abcd1234')
+expect(value.ispn).toHaveLength(8)
 
 
 
